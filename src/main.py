c6e0d83f9c2fc5b5a1e6c524e227a0831ebf07a5
RamonGiovane <ramon.giovane@gmail.com>
2024-10-14 17:30:35 -0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import seaborn as sns
+import yellowbrick
+
+print(f'Versão do pandas: {pd.__version__}')
+print(f'Versão do numpy: {np.__version__}')
+print(f'Versão do scikit-learn (sklearn): {sklearn.__version__}')
+print(f'Versão do seaborn: {sns.__version__}')
+print(f'Versão do yellowbrick: {yellowbrick.__version__}')
